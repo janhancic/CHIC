c22903bd0d4e818d7ff974b0794bc85808026936
root <root@miraculix.caplin.com>
2013-11-03 17:36:37 +0000

    initial project setup without arduino ide and libraries

---
+++ Makefile
@@ -0,0 +1,14 @@
+CC=avr-gcc
+
+compile: chic.c
+	$(CC) -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o chic.o chic.c
+	$(CC) -mmcu=atmega328p chic.o -o chic
+
+upload: chic
+	avr-objcopy -O ihex -R .eeprom chic chic.hex
+	avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM3 -b 115200 -U flash:w:chic.hex
+
+clean: 
+	rm -f *.o
+	rm -f *.hex
+	rm -f chic
--- chic.c
+++ chic.c
@@ -1,39 +1,25 @@
-#define NUM_LEDS 8
-
-int leds[NUM_LEDS] = {2, 3, 4, 5, 6, 7, 8, 9};
-int currentIndex = 0;
-boolean up = true;
-
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize the pins as output pins
-  for (int i = 0; i < NUM_LEDS; i++) {
-      pinMode(leds[i], OUTPUT);
-  }
-
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-      digitalWrite(leds[i], LOW);
-  }
-
-  digitalWrite(leds[currentIndex], HIGH);
+#include <avr/io.h>
+#include <util/delay.h>
 
 
-  if ( up ) {
-    currentIndex++;
-    if ( currentIndex >= 7 ) {
-      up = false;
-    }
-  } else {
-    currentIndex--;
-    if ( currentIndex == 0 ) {
-      up = true;
-    }
-  }
+enum {
+	BLINK_DELAY_MS = 1000,
+};
 
-  delay (250);
+int main (void) 
+{
+	/* set pin 5 of PORTB for output*/
+	DDRB |= _BV(DDB5);
+	while(1)
+	{
+		/* set pin 5 high to turn led on */
+		PORTB |= _BV(PORTB5);
+		_delay_ms(BLINK_DELAY_MS);
+
+		/* set pin 5 low to turn led off */
+		PORTB &= ~_BV(PORTB5);
+		_delay_ms(BLINK_DELAY_MS);
+	}
+
+	return 0;
 }
