5b1efbe1aab91bcaf1fe4a142202de7dd2cba236
Christoph Sitter <christoph.sitter@gmail.com>
2013-12-17 23:10:04 +0000

    updated build. it's compiling all the libraries, but fails with a weird error NUL character seen; ...

--- Makefile
+++ Makefile
@@ -6,16 +6,18 @@ CPU_SPEED	=16000000UL
 CF_ALL		= -Wall -mmcu=$(MCU) -DF_CPU=$(CPU_SPEED) -Os
 LF_ALL		=
 LL_ALL		=
+AF_ALL		= rcs
 UP_ALL		= -V -F -p m328p -c arduino -b 115200 -P$(PORT)
 
 
 ### Build tools
 CC			= avr-gcc
-ARCH		= avr-ar
+AR			= avr-ar
 AVRDUDE		= avrdude
 COMP		= $(CC) $(CF_ALL) $(CF_TGT) -o $@ -c $<
 LINK		= $(CC) $(LF_ALL) $(LF_TGT) -o $@ $^ $(LL_LGT) $(LL_ALL)
 COMPLINK	= $(CC) $(CF_ALL) $(CF_TGT) $(LF_TGT) -o $@ $< $(LL_TGT) $(LL_ALL)
+ARCH		= $(AR) $(AF_ALL) $@ $^
 UPLOAD		= $(AVRDUDE) $(UP_ALL)
 
 
@@ -24,15 +26,15 @@ ARDUINO_VARIANT=$(ARDUINO_DIR)/hardware/arduino/variants/standard
 ### Standard parts
 include Rules.mk
 
-
-
-
-
 BOARD=arduino
 LIBNAME=lib$(BOARD).a
 
 PORT=/dev/cu.usbmodemfd131
 
+
+OBJECTS	:= wiring.o wiring_analog.o wiring_digital.o \
+				wiring_pulse.o wiring_shift.o HardwareSerial.o Print.o   \
+				Tone.o WMath.o WString.o WInterrupts.o
 
 #TGT_UP = chic.hex
 #$(TGT_UP) : chic.elf
@@ -41,6 +43,8 @@ PORT=/dev/cu.usbmodemfd131
 #chic.elf : src/chic.cpp $(LIBNAME)
 #	$(CC) $(CF_ALL) $^ -o $@ -I$(ARDUINO_CORE) -I$(ARDUINO_VARIANT) -Wall
 #	$(COMPLINK) -I$(ARDUINO_CORE) -I$(ARDUINO_VARIANT) $(LIBNAME)
+
+#all : $(LIBNAME)
 
 #$(LIBNAME): $(OBJECTS)
 #	avr-ar rcs $(LIBNAME) $^
--- lib/core/Rules.mk
+++ lib/core/Rules.mk
@@ -9,32 +9,27 @@ d			:= $(dir)
 ARDUINO_CORE=$(ARDUINO_DIR)/hardware/arduino/cores/arduino
 ARDUINO_VARIANT=$(ARDUINO_DIR)/hardware/arduino/variants/standard
 
-OBJS_$(d)	:= $(d)/wiring.o $(d)/wiring_analog.o $(d)/wiring_digital.o \
+TGTS_$(d)	:= $(d)/libcore.a
+DEPS_$(d)	:= $(d)/wiring.o $(d)/wiring_analog.o $(d)/wiring_digital.o \
 				$(d)/wiring_pulse.o $(d)/wiring_shift.o $(d)/HardwareSerial.o $(d)/Print.o   \
 				$(d)/Tone.o $(d)/WMath.o $(d)/WString.o $(d)/WInterrupts.o
 
-CLEAN		:= $(CLEAN) $(OBJS_$(d)) 
-
-# Local rules
-
-TGTS_$(d)	:= $(d)/libcore.a
-DEPS_$(d)	:= $(OBJS_$(d))
-SRC_FROM	:= $(ARDUINO_CORE)
-
-CF_TGT		:= -I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE)
-
-TGT_LIB		:= $(TGT_BIN) $(DEPS_$(d))
+TGT_LIB		:= $(TGT_LIB) $(TGTS_$(d))
 
 CLEAN		:= $(CLEAN) $(TGTS_$(d)) $(DEPS_$(d)
 
-$(OBJS_$(d)):	$(ARDUINO_CORE)/%.c
-	$(COMP) -I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE)
+# Local rules
 
-#$(OBJS_$(d)):			$(ARDUINO_CORE)/%.cpp
-#	$(COMP) -I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE)
+$(TGTS_$(d)):	$(DEPS_$(d))
+		$(ARCH)
 
-#$(d)/$(BOARD)_core.a:	$(OBJS_$(d))
-#	$(ARCH)
+$(d)/%.o:	CF_TGT := -I$(ARDUINO_CORE)
+$(d)/%.o:	$(ARDUINO_CORE)/%.c
+				$(COMP)
+
+$(d)/%.o:	CF_TGT := -I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE)
+$(d)/%.o:	$(ARDUINO_CORE)/%.cpp
+				$(COMP)
 	
 # Standard things
 
