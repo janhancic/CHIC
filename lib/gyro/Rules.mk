d63fa76c057fdc30e8dc70c7be2b57a4726a5b37
Christoph Sitter <christoph.sitter@gmail.com>
2014-01-20 23:10:49 +0000

    fixed gyroscope MPU6050 libarry build

--- lib/gyro/Rules.mk
+++ lib/gyro/Rules.mk
@@ -1,20 +1,21 @@
 # Standard things
 
 sp					:= $(sp).x
-dirstack_$(sp)	:= $(d)
+dirstack_$(sp)		:= $(d)
 d					:= $(dir)
 
 # Local variables
 
-OBJS_$(d)	:= $(d)/mpu6050.o 
-
-CLEAN		:= $(CLEAN) $(OBJS_$(d))
+TGTS_$(d)	:= $(d)/MPU6050.o 
+TGT_LIB		:= $(TGT_LIB) $(TGTS_$(d))
+CLEAN		:= $(CLEAN) $(TGTS_$(d))
 
 # Local rules
-#-I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE)
-$(d)/mpu6050.o:	CF_TGT := -I$(d)/*.h -I$(d)I2Cdev/I2Cdev.h -I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE)
-$(d)/mpu6050.o:	$(d)/MPU6050.cpp $(d)/I2Cdev/I2Cdev.cpp	
-				$(COMP)
+
+$(TGTS_$(d)):	CF_TGT := -I$(ARDUINO_VARIANT) -I$(ARDUINO_CORE) -I$(ARDUINO_DIR)hardware/tools/include -Ilib/i2cdev
+$(TGTS_$(d)):	ARDUINO_VERSION := $(shell grep 'ARDUINO' ${ARDUINO_DIR}/revisions.txt | head -n 1 | awk '{print $$2}' | sed 's/\.//g')
+$(TGTS_$(d)):	$(TGTS_$(d):.o=.cpp) lib/i2cdev/I2CDev.o
+	$(COMP) -D ARDUINO=$(ARDUINO_VERSION)
 	
 # Standard things
 
