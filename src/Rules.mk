4fd762d3d17c6955a770a9f7eaa6abb2ac9a5877
Christoph Sitter <christoph.sitter@caplin.com>
2014-01-28 22:02:35 +0000

    can use the libraries on my fedora laptop, build hard coded - needs fixing - MPU6050 libraries work and we get data from the gyroscope

--- src/Rules.mk
+++ src/Rules.mk
@@ -11,13 +11,13 @@ TGTS_$(d)	:= $(d)/chic.hex
 TGT_BIN		:= $(TGT_BIN) $(TGTS_$(d))
 CLEAN		:= $(CLEAN) $(TGTS_$(d)) $(TGTS_$(d):%.hex=%.elf)
 
-$(TGTS_$(d)): $(TGTS_$(d):%.hex=%.elf) $(d)/dummytest.o 
+$(TGTS_$(d)): $(TGTS_$(d):%.hex=%.elf) 
 			$(OBJCPY) -O ihex
 
-$(TGTS_$(d):%.hex=%.elf):	CF_TGT := -I$(ARDUINO_CORE) -I$(ARDUINO_VARIANT)
-$(TGTS_$(d):%.hex=%.elf):	LL_TGT := lib/core/libcore.a
-$(TGTS_$(d):%.hex=%.elf):	$(d)/chic.cpp lib/core/libcore.a
-							$(COMPLINK)
+$(TGTS_$(d):%.hex=%.elf):	CF_TGT := -I$(ARDUINO_CORE) -I$(ARDUINO_VARIANT) -I/home/chsitter/Development/chic/lib/i2cdev -I/home/chsitter/Development/chic/lib/gyro
+$(TGTS_$(d):%.hex=%.elf):	LL_TGT := lib/core/libcore.a lib/gyro/MPU6050.o lib/i2cdev/I2CDev.o
+$(TGTS_$(d):%.hex=%.elf):	$(d)/chic.cpp lib/core/libcore.a 
+							$(COMPLINK) -I/lib/i2cdev
 
 
 $(d)/dummytest.o: $(d)/tests/dummytest.c
