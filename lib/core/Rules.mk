295685416d5291d3e8bc03804c60b789ba9d0ae9
Christoph Sitter <christoph.sitter@gmail.com>
2014-03-19 21:09:05 +0000

    added Serial library to build

--- lib/core/Rules.mk
+++ lib/core/Rules.mk
@@ -13,13 +13,14 @@ OBJS_$(d)	:= $(d)/CDC.o $(d)/HID.o $(d)/wiring.o $(d)/wiring_analog.o \
 				$(d)/Tone.o $(d)/WMath.o $(d)/WInterrupts.o $(d)/HID.o $(d)/Tone.o \
 				$(d)/malloc.o $(d)/realloc.o $(d)/WString.o
 
-LIBS_$(d)   := $(d)/Wire.o $(d)/twi.o
+LIBS_$(d)   := $(d)/Wire.o $(d)/twi.o $(d)/Servo.o
 
 TGT_LIB		:= $(TGT_LIB) $(TGTS_$(d))
 CLEAN			:= $(CLEAN) $(TGTS_$(d)) $(OBJS_$(d))
 
 vpath % $(ARDUINO_CORE):$(ARDUINO_CORE)/avr-libc
 vpath % $(ARDUINO_DIR)/libraries/Wire:$(ARDUINO_DIR)/libraries/Wire/utility
+vpath % $(ARDUINO_DIR)/libraries/Servo
 # Local rules
 
 $(TGTS_$(d)):	$(OBJS_$(d)) $(LIBS_$(d)) 
