fb6352fe13492a5e433e4358c282cdb89173c0e0
Christoph Sitter <christoph.sitter@gmail.com>
2014-05-04 10:24:26 +0100

    integrated gmock with build

--- lib/gmock/Rules.mk
+++ lib/gmock/Rules.mk
@@ -10,10 +10,13 @@ TGTS_$(d)	:= $(d)/gmock_main.a $(d)/gmock.a $(d)/*.o
 TGT_LIB		:= $(TGT_LIB) $(TGTS_$(d))
 CLEAN			:= $(CLEAN) $(TGTS_$(d))
 
-GTEST_DIR 	:= $(d)/gmock-1.7.0/gtest
-GMOCK_DIR 	:= $(d)/gmock-1.7.0 
+GTEST_DIR 	:= $(d)/gmock/gtest
+GMOCK_DIR 	:= $(d)/gmock
 CPPFLAGS 	:= -isystem $(GTEST_DIR)/include -isystem $(GMOCK_DIR)/include
 CXXFLAGS 	:= -g -Wall -Wextra -pthread
+
+GTEST_HEADERS = $(GTEST_DIR)/include/gtest/*.h \
+                $(GTEST_DIR)/include/gtest/internal/*.h
 
 GMOCK_HEADERS := $(GMOCK_DIR)/include/gmock/*.h \
                 $(GMOCK_DIR)/include/gmock/internal/*.h \
@@ -22,28 +25,29 @@ GMOCK_HEADERS := $(GMOCK_DIR)/include/gmock/*.h \
 GTEST_SRCS_ := $(GTEST_DIR)/src/*.cc $(GTEST_DIR)/src/*.h $(GTEST_HEADERS)
 GMOCK_SRCS_ := $(GMOCK_DIR)/src/*.cc $(GMOCK_HEADERS)
 
+
 # Local rules
 $(d)/gtest-all.o : $(GTEST_SRCS_)
 	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) -I$(GMOCK_DIR) $(CXXFLAGS) \
-            -c $(GTEST_DIR)/src/gtest-all.cc
+            -o $@ -c $(GTEST_DIR)/src/gtest-all.cc
 
 $(d)/gmock-all.o : $(GMOCK_SRCS_)
 	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) -I$(GMOCK_DIR) $(CXXFLAGS) \
-            -c $(GMOCK_DIR)/src/gmock-all.cc
+            -o $@ -c $(GMOCK_DIR)/src/gmock-all.cc
 
 $(d)/gmock_main.o : $(GMOCK_SRCS_)
 	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) -I$(GMOCK_DIR) $(CXXFLAGS) \
-            -c $(GMOCK_DIR)/src/gmock_main.cc
+            -o $@ -c $(GMOCK_DIR)/src/gmock_main.cc
 
-$(d)/gmock.a : $(d)/gmock-all.o $(d)/gtest-all.o
-	$(AR) $(ARFLAGS) $@ $^
+$(d)/gmock.a : lib/gmock/gmock-all.o lib/gmock/gtest-all.o
+	ar $(ARFLAGS) $@ $^
 
-$(d)/gmock_main.a : $(d)/gmock-all.o $(d)/gtest-all.o $(d)/gmock_main.o
-	$(AR) $(ARFLAGS) $@ $^
+$(d)/gmock_main.a : lib/gmock/gmock-all.o lib/gmock/gtest-all.o lib/gmock/gmock_main.o
+	ar $(ARFLAGS) $@ $^
 
 # Standard things
 
 -include	$(DEPS_$(d))
 
 d			:= $(dirstack_$(sp))
-sp			:= $(basename $(sp)):
+sp			:= $(basename $(sp))
